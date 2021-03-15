9f7c969c903f0b5dfb0e19d4d77575fca9043333
wb-yjw637332 <wb-yjw637332@alibaba-inc.com>
2021-03-15 15:12:15 +0800

    C++简单文件IO流使用

---
+++ chapter_006_io/cpp_study_0001.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 杨建伟 on 2021/3/15.
+//
+
+# include <iostream>
+# include <climits>
+# include <string>
+# include <cstring>
+# include <vector>
+# include <array>
+# include <ctime>
+# include <cctype>
+# include <cmath>
+
+using namespace std;
+
+int main() {
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+
+    return 0;
+}
+
+
---
+++ cmake-build-debug/CMakeFiles/cpp_study.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_study.dir/chapter_006_io/cpp_study_0001.cpp.obj"
+  "cpp_study.exe"
+  "cpp_study.exe.manifest"
+  "cpp_study.pdb"
+  "libcpp_study.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
