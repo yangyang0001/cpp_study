8a559aba72bea228a8befd605c3bd767f9d75c98
yangjianwei <wb-yjw637332@alibaba-inc.com>
2021-03-19 19:26:50 +0800

    C++静态常量的零初始化_常量表达式初始化_常量的动态初始化

---
+++ chapter_009/cpp_study_0004.cpp
@@ -0,0 +1,32 @@
+//
+// Created by 杨建伟 on 2021/3/19.
+//
+
+# include <iostream>
+# include <climits>
+# include <string>
+# include <cstring>
+# include <vector>
+# include <array>
+# include <ctime>
+# include <cctype>
+# include <cmath>
+# include <fstream>
+
+using namespace std;
+
+int x;
+int y = 5;
+long z = 13 * 13;
+const double PI = 4.0 * atan(1.0);
+
+
+int main() {
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+
+    cout << "atan(1.0) = " << atan(1.0) << endl;
+
+    return 0;
+}
+
+
--- cmake-build-debug/CMakeFiles/cpp_study.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/cpp_study.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "D:/CLionProjects/cpp_study/chapter_009/cpp_study_0003.cpp" "D:/CLionProjects/cpp_study/cmake-build-debug/CMakeFiles/cpp_study.dir/chapter_009/cpp_study_0003.cpp.obj"
+  "D:/CLionProjects/cpp_study/chapter_009/cpp_study_0004.cpp" "D:/CLionProjects/cpp_study/cmake-build-debug/CMakeFiles/cpp_study.dir/chapter_009/cpp_study_0004.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
