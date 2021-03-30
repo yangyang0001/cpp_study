87997cfe608eb092ad33ad3a2756df2782fa2b3c
yangjianwei <wb-yjw637332@alibaba-inc.com>
2021-03-30 11:48:50 +0800

    C++_类和对象_练习题

--- cmake-build-debug/CMakeFiles/cpp_study.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/cpp_study.dir/cmake_clean.cmake
@@ -1,12 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cpp_study.dir/chapter_010/cpp_lianxi_002_impl.cpp.obj"
-  "cpp_study.exe"
-  "cpp_study.exe.manifest"
-  "cpp_study.pdb"
-  "libcpp_study.dll.a"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/cpp_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
