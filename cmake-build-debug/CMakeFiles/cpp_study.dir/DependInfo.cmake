9a99760d147b5aa397fd9a43610740b06c9ae374
yangjianwei <wb-yjw637332@alibaba-inc.com>
2021-04-03 15:32:38 +0800

    友元函数

--- cmake-build-debug/CMakeFiles/cpp_study.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/cpp_study.dir/DependInfo.cmake
@@ -1,17 +1,7 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/yangjianwei/CLionProjects/cpp_study/chapter_011/cpp_study_0003.cpp" "/Users/yangjianwei/CLionProjects/cpp_study/cmake-build-debug/CMakeFiles/cpp_study.dir/chapter_011/cpp_study_0003.cpp.o"
-  "/Users/yangjianwei/CLionProjects/cpp_study/chapter_011/cpp_study_0003_impl.cpp" "/Users/yangjianwei/CLionProjects/cpp_study/cmake-build-debug/CMakeFiles/cpp_study.dir/chapter_011/cpp_study_0003_impl.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
--- cmake-build-debug/CMakeFiles/cpp_study.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/cpp_study.dir/cmake_clean.cmake
@@ -1,11 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cpp_study.dir/chapter_011/cpp_study_0003.cpp.o"
-  "CMakeFiles/cpp_study.dir/chapter_011/cpp_study_0003_impl.cpp.o"
-  "cpp_study"
-  "cpp_study.pdb"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/cpp_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
