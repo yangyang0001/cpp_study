2ecc8b3a944bbe7ca90da81a97d1916ac78af00d
yangjianwei <wb-yjw637332@alibaba-inc.com>
2021-04-05 19:08:35 +0800

    C++中类与动态内存分配

---
+++ chapter_012/cpp_study_0001_impl.cpp
@@ -0,0 +1,49 @@
+//
+// Created by 杨建伟 on 2021/4/5.
+//
+
+# include <iostream>
+# include <climits>
+# include <string>
+# include <cstring>
+# include <vector>
+# include <array>
+# include <ctime>
+# include <cctype>
+# include <cmath>
+# include <fstream>
+# include "cpp_study_0001.h"
+
+using namespace std;
+
+// 在 header 文件中, 使用的静态变量的初始化
+int StringBad::num_strings = 0;
+
+StringBad::StringBad() {
+    this->length = 3;
+    this->str = new char[4];
+    strcpy(str, "C++");
+    str[length] = '\0';
+    num_strings ++;
+    cout << "none param constructor invoked, str = " << this->str << ", length = " << this->length << ", number_strings = " << this->num_strings << endl;
+}
+
+StringBad::StringBad(const char * s) {
+    this->length = strlen(s);
+    this->str = new char[this->length + 1];
+    strcpy(str, s);
+    str[length] = '\0';
+    num_strings ++;
+    cout << "have param constructor invoked, str = " << this->str << ", length = " << this->length << ", number_strings = " << this->num_strings << endl;
+}
+
+StringBad::~StringBad() {
+    num_strings --;
+    cout << "destructor ~StringBad() invoked, str = " << this->str << ", length = " << this->length << ", number_strings = " << this->num_strings << endl;
+    delete[] this->str;
+}
+
+ostream & operator << (ostream & os, const StringBad & stringBad) {
+    os << stringBad.str << endl;
+    return os;
+}
--- cmake-build-debug/CMakeFiles/cpp_study.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/cpp_study.dir/cmake_clean.cmake
@@ -1,5 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_study.dir/chapter_012/cpp_study_0001.cpp.obj"
+  "CMakeFiles/cpp_study.dir/chapter_012/cpp_study_0001_impl.cpp.obj"
+  "cpp_study.exe"
+  "cpp_study.exe.manifest"
+  "cpp_study.pdb"
+  "libcpp_study.dll.a"
+)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang )
+foreach(lang CXX)
   include(CMakeFiles/cpp_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
