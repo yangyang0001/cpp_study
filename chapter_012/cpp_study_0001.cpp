c609da12c7d5a94c222f5b585b77792642e98bbc
yangjianwei <2017585616@qq.com>
2021-04-09 01:45:34 +0800

    C++类与动态内存分配

--- chapter_012/cpp_study_0001.cpp
+++ chapter_012/cpp_study_0001.cpp
@@ -22,39 +22,40 @@ void callme2(StringBad stringBad);
 int main() {
     cout.setf(ios_base::fixed, ios_base::floatfield);
     {
-        cout << "Starting inner block!" << endl;
-        StringBad headline1("Celery Stalks at Midnight");
-        StringBad headline2("Lettuce Prey");
-        StringBad sports("Spinach Leaves Bowl for Dollars");
+        cout << "Starting inner block!" << endl;            //
+        StringBad headline1("Celery Stalks at Midnight");   // 显式构造函数   +1  num_strings = 1
+        StringBad headline2("Lettuce Prey");                // 显式构造函数   +1  num_strings = 2
+        StringBad sports("Spinach Leaves Bowl for Dollars");// 显式构造函数   +1  num_strings = 3
 
-        cout << "headline1 = " << headline1 << endl;
-        cout << "headline2 = " << headline2 << endl;
-        cout << "sports    = " << sports << endl;
+        cout << "headline1 = " << headline1 << endl;        //
+        cout << "headline2 = " << headline2 << endl;        //
+        cout << "sports    = " << sports << endl;           //
 
         cout << "1 ----------------------------------------------------------------------------------" << endl;
 
-        callme1(headline1);
-        cout << "headline1 = " << headline1 << endl;
+        callme1(headline1);                              // 不会触发隐式复制构造函数, 因为是引用传递
+        cout << "headline1 = " << headline1 << endl;        //
 
         cout << "2 ----------------------------------------------------------------------------------" << endl;
 
-        callme2(headline2);
-        cout << "headline2 = " << headline2 << endl;
+        callme2(headline2);                                 // 触发隐式复制构造函数, 产生副本后一旦过期 触发析构函数调用 -1 num_strings = 2
+        cout << "headline2 = " << headline2 << endl;        //
 
         cout << "3 ----------------------------------------------------------------------------------" << endl;
 
-        cout << "Initialize one object to another:" << endl;
-        StringBad sailor = sports;
-        cout << "sailor = " << sailor << endl;
+        cout << "Initialize one object to another:" << endl;//
+        StringBad sailor = sports;                          // 触发隐式复制构造函数
+        cout << "sailor = " << sailor << endl;              //
 
         cout << "4 ----------------------------------------------------------------------------------" << endl;
 
-        cout << "Assgin one object to another:" << endl;
-        StringBad knot = headline1;
+        cout << "Assgin one object to another:" << endl;    //
+        StringBad knot = headline1;                         // 触发隐式复制构造函数
         cout << "knot = " << knot << endl;
         cout << "Exit the block!" << endl;
 
         cout << "5 ----------------------------------------------------------------------------------" << endl;
+        // 结束后 按照 knot, sailor, sports, headline2, headline1 回收
     }
     cout << "End of main()!" << endl;
     return 0;
