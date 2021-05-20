5ef65293d52d7f581267702ee526d30569dfdf47
wb-yjw637332 <wb-yjw637332@alibaba-inc.com>
2021-05-20 20:03:24 +0800

    C语言HelloWorld

--- cmake-build-debug/CMakeFiles/cpp_study.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/cpp_study.dir/DependInfo.cmake
@@ -5,17 +5,17 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Users/yangjianwei/CLionProjects/cpp_study/HelloWorld.c" "/Users/yangjianwei/CLionProjects/cpp_study/cmake-build-debug/CMakeFiles/cpp_study.dir/HelloWorld.c.o"
+  "D:/CLionProjects/cpp_study/HelloWorld.c" "D:/CLionProjects/cpp_study/cmake-build-debug/CMakeFiles/cpp_study.dir/HelloWorld.c.obj"
   )
-set(CMAKE_C_COMPILER_ID "AppleClang")
+set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
   )
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/yangjianwei/CLionProjects/cpp_study/chapter_012/cpp_study_0002_impl.cpp" "/Users/yangjianwei/CLionProjects/cpp_study/cmake-build-debug/CMakeFiles/cpp_study.dir/chapter_012/cpp_study_0002_impl.cpp.o"
+  "D:/CLionProjects/cpp_study/chapter_012/cpp_study_0002_impl.cpp" "D:/CLionProjects/cpp_study/cmake-build-debug/CMakeFiles/cpp_study.dir/chapter_012/cpp_study_0002_impl.cpp.obj"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
